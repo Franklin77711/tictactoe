2e90f80ed2b87e0c74409ae4a783260bf049073b
Franklin77711 <robi1000350@gmail.com>
2022-11-02 16:58:02 +0100

    need to add turns, AI and check win

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 const playarea = document.getElementById("playare");
 const squares = document.querySelectorAll(".square");
 const restartBtn = document.getElementById("reset");
+const winCondition = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
 
 //user input X
 for (const square of squares) {
     square.addEventListener("click", function addX(){
+        if(square.textContent == ""){
         square.textContent = "X";
+        square.setAttribute("class", "X square");
+    }
 })
 
 }
 function addO(){
-
+    
 }
 
 //clear
@@ -20,3 +33,11 @@ function clear(){
     } 
 }
 restartBtn.addEventListener("click", clear);
+//check win
+function checkWin() {
+    return winCondition.some(combination => {
+      return combination.every(index => {
+        return (squares[index].classList.contains("X"))
+      }
+    )})
+    }
