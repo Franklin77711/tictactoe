ec8ee62d7567ff0cb3dc91edebf9e7a25674d7c9
Franklin77711 <robi1000350@gmail.com>
2022-11-02 23:00:00 +0100

    Only the AI remains

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-const playarea = document.getElementById("playare");
+const playarea = document.getElementById("playarea");
 const squares = document.querySelectorAll(".square");
-const restartBtn = document.getElementById("reset");
+const restartBtn = document.getElementById("restart");
+const anotherRound = document.getElementById("reset");
+const popUp = document.getElementById("popup");
+const overlay = document.querySelector(".overlay");
 const winCondition = [
     [0,1,2],
     [3,4,5],
@@ -11,13 +14,26 @@ const winCondition = [
     [0,4,8],
     [2,4,6]
 ]
+/*let board;
+
+startGame();
+
+function startGame(){
+    for (const square of squares) {
+        square.textContent="";
+        board = Array.from(Array(9).keys());
+    }
+}*/
 
 //user input X
 for (const square of squares) {
     square.addEventListener("click", function addX(){
         if(square.textContent == ""){
         square.textContent = "X";
-        square.setAttribute("class", "X square");
+            if (checkWin()== true){
+                popUp.style.visibility = "visible";
+                overlay.classList.add("active")
+        }
     }
 })
 
@@ -29,15 +45,18 @@ function addO(){
 //clear
 function clear(){
     for (let i = 0; i < squares.length; i++) {
-        squares[i].textContent="";  
+        squares[i].textContent="";
     } 
+    popUp.style.visibility = "hidden";
+    overlay.classList.remove("active");
 }
 restartBtn.addEventListener("click", clear);
+anotherRound.addEventListener("click", clear);
 //check win
 function checkWin() {
     return winCondition.some(combination => {
       return combination.every(index => {
-        return (squares[index].classList.contains("X"))
+        return (squares[index].textContent=="X" || squares[index].textContent=="O")
       }
     )})
     }
